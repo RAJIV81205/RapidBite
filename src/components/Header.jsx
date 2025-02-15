c0a4c5f5b10d935c6c6c9cbabeac0d77f89b9396
Rajiv <lucky81205@gmail.com>
2025-02-15 10:53:34 +0530

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
+
+
 function Header(){
     return(
-        <header>
-            
+        <header className="fixed bg-(--color-background) w-full h-[12vh] border-b-2 border-(--color-neutral)/15 flex-row">
+            <div className="w-">RapidBite</div>
+
         </header>
     );
 }
