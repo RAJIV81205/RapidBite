2928d68ff5abeb88b4a7dc379f49d1de5864553f
Rajiv <lucky81205@gmail.com>
2025-02-15 23:16:49 +0530

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,8 +18,8 @@ function Header() {
   const time = t >= 10 ? t : `0${t}`;
 
   return (
-    <header className="fixed bg-background w-full h-[12vh] border-b-2 border-neutral/15 flex flex-row justify-center items-center">
-      <div className="w-fit flex justify-center items-center h-full px-[20px]">
+    <header className="fixed bg-background w-full h-[12vh] border-b-2 border-neutral/15 flex flex-row justify-evenly items-center">
+      <div className="w-fit flex justify-center items-center h-full px-[20px] border-r-neutral/15 border-r-1 ">
         <span className="text-secondary font-noto text-3xl not-italic tracking-wide font-medium">
           Rapid
         </span>
@@ -27,18 +27,20 @@ function Header() {
           Bite
         </span>
       </div>
-      <div className="h-full w-fit border-neutral/15 border-r-1 border-l-1 flex flex-col justify-center items-center px-[20px]">
+      <div className="h-full w-fit flex flex-col justify-center items-center px-[20px]">
         <span className="text-xl font-roboto font-semibold">Delivering in {time} Minutes</span>
         <span className="text-xl font-roboto">
           {lat && lon ? `${lat}, ${lon}` : "Fetching location..."}
         </span>
       </div>
-      <div className="w-[50%] flex justify-center items-center">
+      <div className="w-[50%] flex justify-center items-center relative">
+        <img src="public/search.png" alt="search" className="absolute aspect-square invert-50 left-[55px] h-[20px]"/>
         <input
           type="text"
           placeholder="Search milk"
-          className="w-[90%] border-1 border-neutral/15 py-2.5 rounded-3xl pl-5 bg-neutral/5 focus:outline-neutral/50"
+          className="w-[90%] border-1 border-neutral/15 py-2.5 rounded-3xl pl-12 bg-neutral/5 focus:outline-neutral/50"
         />
+        <div className="">Login</div>
       </div>
     </header>
   );
