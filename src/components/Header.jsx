605f0f90266083e3ee582d8b51344d2249aa9892
Rajiv <lucky81205@gmail.com>
2025-02-15 16:11:42 +0530

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,4 @@
 function Header() {
-
-
   return (
     <header className="fixed bg-background w-full h-[12vh] border-b-2 border-neutral/15 flex flex-row justify-center items-center">
       <div className="w-fit flex justify-center items-center h-full px-[20px]">
@@ -12,9 +10,12 @@ function Header() {
         </span>
       </div>
       <div className="h-full w-fit border-neutral/15 border-r-1 border-l-1 flex flex-col justify-center items-center px-[20px]">
-      <span className="text-xl">Delivering in</span>
-      <span className="text-2xl">{Math.floor(Math.random()*100)} Minutes</span>
+        <span className="text-xl font-roboto">Delivering in {Math.floor(Math.random() * 100)} Minutes</span>
+        <span className="text-xl font-roboto">
+                    
+        </span>
       </div>
+      <div className="w-[50%] flex justify-center items-center"><input type="text" placeholder="Search milk" className="w-[90%] border-1 border-neutral/15 py-2.5 rounded-3xl pl-1.5"/></div>
     </header>
   );
 }
