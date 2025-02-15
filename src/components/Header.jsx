d3041f7a3482ef6a63fbb488088745006ddb3a01
Rajiv <lucky81205@gmail.com>
2025-02-15 14:58:58 +0530

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@
 function Header(){
     return(
         <header className="fixed bg-(--color-background) w-full h-[12vh] border-b-2 border-(--color-neutral)/15 flex-row">
-            <div className="w-">RapidBite</div>
+            <div className="w-[20vh] flex justify-center items-center h-full"><span>Rapid</span><span>Bite</span></div>
 
         </header>
     );
